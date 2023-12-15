02e474eefeda13956151ddfaf656d213c33fef41
om <omejjigiri99@gmail.com>
2023-12-15 20:15:48 +0530

    making shell scripting topics - little heading underlining

---
+++ mysql.sh
@@ -0,0 +1,12 @@
+dnf module disable mysql -y
+
+dnf install mysql-community-server -y
+
+cp mysql.repo /etc/yum.repos.d/mysql.repo
+
+systemctl enable mysqld
+
+systemctl start mysqld
+
+mysql_secure_installation --set-root-pass ExpenseApp@1
+
