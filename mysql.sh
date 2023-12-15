3a398a4e0b5dbd9ddbd1612081c95b417df7979e
om <omejjigiri99@gmail.com>
2023-12-15 22:41:12 +0530

    making shell scripting topics - little heading underlining

--- mysql.sh
+++ mysql.sh
@@ -1,13 +1,13 @@
 echo -e "\e[36m disable the mysql \e[0m"
-dnf module disable mysql -y
+dnf module disable mysql -y &>>/tmp/expense.log
 echo -e "\e[36m cp mysql.repo \e[0m"
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>>/tmp/expense.log
 echo -e "\e[36m Installing the nginx \e[0m"
-dnf install mysql-community-server -y
+dnf install mysql-community-server -y &>>/tmp/expense.log
 echo -e "\e[36m enable the mysql \e[0m"
-systemctl enable mysqld
+systemctl enable mysqld &>>/tmp/expense.log
 echo -e "\e[36m start the mysqld \e[0m"
-systemctl start mysqld
+systemctl start mysqld &>>/tmp/expense.log
 echo -e "\e[36m setting the password for db \e[0m"
-mysql_secure_installation --set-root-pass ExpenseApp@1
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>/tmp/expense.log
 
