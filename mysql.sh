d4d3fbe9355d2e6a3359714f654aec636d5e169a
om <omejjigiri99@gmail.com>
2023-12-16 14:03:25 +0530

    making shell scripting topics - little heading underlining

--- mysql.sh
+++ mysql.sh
@@ -3,20 +3,56 @@ colour="\e[36m"
 
 echo -e "${colour} disable the mysql \e[0m"
 dnf module disable mysql -y &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo success
+  echo -e "\e[32m SUCCESS \e[0m"
+else
+  echo failure
+  echo -e "\e[32m FAILURE \e[0m"
+fi
 echo -e "${colour} cp mysql.repo \e[0m"
 cp mysql.repo /etc/yum.repos.d/mysql.repo &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo success
+  echo -e "\e[32m SUCCESS \e[0m"
+else
+  echo failure
+  echo -e "\e[32m FAILURE \e[0m"
+fi
 echo -e "${colour} Installing the nginx \e[0m"
 dnf install mysql-community-server -y &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo success
+  echo -e "\e[32m SUCCESS \e[0m"
+else
+  echo failure
+  echo -e "\e[32m FAILURE \e[0m"
+fi
 echo -e "${colour} enable the mysql \e[0m"
 systemctl enable mysqld &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo success
+  echo -e "\e[32m SUCCESS \e[0m"
+else
+  echo failure
+  echo -e "\e[32m FAILURE \e[0m"
+fi
 echo -e "${colour} start the mysqld \e[0m"
 systemctl start mysqld &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo success
+  echo -e "\e[32m SUCCESS \e[0m"
+else
+  echo failure
+  echo -e "\e[32m FAILURE \e[0m"
+fi
 echo -e "${colour} setting the password for db \e[0m"
 mysql_secure_installation --set-root-pass ExpenseApp@1 &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo success
+  echo -e "\e[32m SUCCESS \e[0m"
+else
+  echo failure
+  echo -e "\e[32m FAILURE \e[0m"
+fi
 
