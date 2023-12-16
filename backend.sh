78ff74e4b5b6be092313a4621664cdcd361183dd
om <omejjigiri99@gmail.com>
2023-12-16 15:10:49 +0530

    making shell scripting topics - little heading underlining

--- backend.sh
+++ backend.sh
@@ -4,37 +4,29 @@ colour="\e[36m"
 echo -e "${colour} disable the nodejs \e[0m"
 dnf module disable nodejs -y &>>log_file
 if [ $? -eq 0 ]; then
-  echo success
   echo -e "\e[32m SUCCESS \e[0m"
 else
-  echo failure
   echo -e "\e[32m FAILURE \e[0m"
 fi
 echo -e "${colour} enable the nodejs \e[0m"
 dnf module enable nodejs:18 -y &>>log_file
 if [ $? -eq 0 ]; then
-  echo success
   echo -e "\e[32m SUCCESS \e[0m"
 else
-  echo failure
   echo -e "\e[32m FAILURE \e[0m"
 fi
 echo -e "${colour} install the nodejs \e[0m"
 dnf install nodejs -y &>>log_file
 if [ $? -eq 0 ]; then
-  echo success
   echo -e "\e[32m SUCCESS \e[0m"
 else
-  echo failure
   echo -e "\e[32m FAILURE \e[0m"
 fi
 echo -e "${colour} copy the backendservice \e[0m"
 cp backend.service  /etc/systemd/system/backend.service &>>log_file
 if [ $? -eq 0 ]; then
-  echo success
   echo -e "\e[32m SUCCESS \e[0m"
 else
-  echo failure
   echo -e "\e[32m FAILURE \e[0m"
 fi
 
@@ -43,20 +35,17 @@ if [ $? -ne 0 ]; then
  echo -e "${colour} adding the expense file \e[0m"
  useradd expense &>>log_file
  if [ $? -eq 0 ]; then
-   echo success
    echo -e "\e[32m SUCCESS \e[0m"
  else
-   echo failure
    echo -e "\e[32m FAILURE \e[0m"
- fi
+  fi
 fi
 
 if [ ! -d /app ]; then
 echo -e "${colour} creating the folder /app \e[0m"
 mkdir /app &>>log_file
  if [ $? -eq 0 ]; then
-   echo success
-   echo -e "\e[32m SUCCESS \e[0m"
+  echo -e "\e[32m SUCCESS \e[0m"
  else
    echo failure
    echo -e "\e[32m FAILURE \e[0m"
@@ -68,65 +57,51 @@ rm -rf /app/* &>>log_file
 echo -e "${colour} download & extract the backend zip file \e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>log_file
 if [ $? -eq 0 ]; then
-  echo success
   echo -e "\e[32m SUCCESS \e[0m"
 else
-  echo failure
   echo -e "\e[32m FAILURE \e[0m"
 fi
 echo -e "${colour} changing the directory to /app \e[0m"
 cd /app &>>log_file
 if [ $? -eq 0 ]; then
-  echo success
   echo -e "\e[32m SUCCESS \e[0m"
 else
-  echo failure
   echo -e "\e[32m FAILURE \e[0m"
 fi
 echo -e "${colour} unzip the backend.zip file \e[0m"
 unzip /tmp/backend.zip &>>log_file
 if [ $? -eq 0 ]; then
-  echo success
   echo -e "\e[32m SUCCESS \e[0m"
 else
-  echo failure
   echo -e "\e[32m FAILURE \e[0m"
 fi
 echo -e "${colour} changing the directory \e[0m"
 cd /app &>>log_file
 npm install &>>log_file
 if [ $? -eq 0 ]; then
-  echo success
   echo -e "\e[32m SUCCESS \e[0m"
 else
-  echo failure
   echo -e "\e[32m FAILURE \e[0m"
 fi
 echo -e "${colour} installing the mysql \e[0m"
 dnf install mysql -y &>>log_file
 if [ $? -eq 0 ]; then
-  echo success
   echo -e "\e[32m SUCCESS \e[0m"
 else
-  echo failure
   echo -e "\e[32m FAILURE \e[0m"
 fi
 echo -e "${colour} mysql username \e[0m"
 mysql -h mysql-dev.osdevops99.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>log_file
 if [ $? -eq 0 ]; then
-  echo success
   echo -e "\e[32m SUCCESS \e[0m"
 else
-  echo failure
   echo -e "\e[32m FAILURE \e[0m"
 fi
 systemctl daemon-reload &>>log_file
 systemctl enable backend &>>log_file
 systemctl restart backend &>>log_file
 if [ $? -eq 0 ]; then
-  echo success
   echo -e "\e[32m SUCCESS \e[0m"
 else
-  echo failure
   echo -e "\e[32m FAILURE \e[0m"
 fi
--- frontend.sh
+++ frontend.sh
@@ -4,67 +4,53 @@ colour="\e[36m"
 echo -e "${colour} Installing nginx \e[0m"
 dnf install nginx -y &>>log_file
 if [ $? -eq 0 ]; then
-  echo success
   echo -e "\e[32m SUCCESS \e[0m"
 else
-  echo failure
   echo -e "\e[32m FAILURE \e[0m"
 fi
 
 echo -e "${colour}copy expense.conf file \e[0m"
 cp expense.conf  /etc/nginx/default.d/expense.conf &>>log_file
 if [ $? -eq 0 ]; then
-  echo success
   echo -e "\e[32m SUCCESS \e[0m"
 else
-  echo failure
   echo -e "\e[32m FAILURE \e[0m"
 fi
 
 echo -e "${colour} Installing nginx \e[0m"
 rm -rf /usr/share/nginx/html/* &>>log_file
 if [ $? -eq 0 ]; then
-  echo success
   echo -e "\e[32m SUCCESS \e[0m"
 else
-  echo failure
   echo -e "\e[32m FAILURE \e[0m"
 fi
 
 echo -e "${colour}download & extract the fronend file \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>log_file
 if [ $? -eq 0 ]; then
-  echo success
   echo -e "\e[32m SUCCESS \e[0m"
 else
-  echo failure
   echo -e "\e[32m FAILURE \e[0m"
 fi
 echo -e "${colour} changing the dir \e[0m"
 cd /usr/share/nginx/html &>>log_file
 if [ $? -eq 0 ]; then
-  echo success
   echo -e "\e[32m SUCCESS \e[0m"
 else
-  echo failure
   echo -e "\e[32m FAILURE \e[0m"
 fi
 echo -e "${colour} Unzip the frontend zip \e[0m"
 unzip /tmp/frontend.zip &>>log_file
 if [ $? -eq 0 ]; then
-  echo success
   echo -e "\e[32m SUCCESS \e[0m"
 else
-  echo failure
   echo -e "\e[32m FAILURE \e[0m"
 fi
 echo -e "${colour} enable & restart the nginx \e[0m"
 systemctl enable nginx &>>log_file
 systemctl restart nginx &>>log_file
 if [ $? -eq 0 ]; then
-  echo success
   echo -e "\e[32m SUCCESS \e[0m"
 else
-  echo failure
   echo -e "\e[32m FAILURE \e[0m"
 fi
