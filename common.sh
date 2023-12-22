fc29d7f57becd6336c52ff21b358ebe45ad29e3b
om <omejjigiri99@gmail.com>
2023-12-22 16:12:50 +0530

    making shell scripting topics - little heading underlining

--- common.sh
+++ common.sh
@@ -12,36 +12,6 @@ fi
 
 
 
-pipeline {
-  agent {
-    node { label 'workstation'}
-  }
-
-  stages {
-
-    stage('compile'){
-      steps {
-        echo 'compile'
-      }
-    }
-
-    stage('Testcases'){
-      steps {
-        echo 'Testcases'
-      }
-    }
-    stage('Build'){
-      steps {
-          echo 'Build'
-      }
-    }
-    stage('Release'){
-      steps {
-          echo 'Release'
-      }
-    }
-  }
-}
 
 
 
